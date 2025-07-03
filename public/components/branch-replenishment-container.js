19450904e8a9d48d12bce97ac41a6e1272253a19
Andrei Manea <office@concept24.ro>
2025-07-03 10:32:45 +0000

    feat: Remove quick panel toggle functionality after applying a strategy

--- public/components/branch-replenishment-container.js
+++ public/components/branch-replenishment-container.js
@@ -366,14 +366,6 @@ export class BranchReplenishmentContainer extends LitElement {
     
     // Don't reset dropdown immediately, user might want to apply again with different successive setting
     // this.selectedReplenishmentStrategy = 'none';
-    this._toggleQuickPanel(false); // Hide quick panel after applying a strategy
-  }
-
-  _toggleQuickPanel(visible) {
-    const quickPanel = this.querySelector('quick-panel');
-    if (quickPanel) {
-      quickPanel.style.display = visible ? '' : 'none';
-    }
   }
 
   _handleResetData() {
